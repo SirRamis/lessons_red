c0f038660949a3e2b96a8a552a24ef82768a6eed
Ramis <sir.ramis@mail.ru>
2024-02-11 16:32:10 +0300

    HW 2

--- HW.py
+++ HW.py
@@ -3,3 +3,11 @@ def paperwork(n, m):
     if n <= 0 or m <= 0:
         return 0
     return n * m
+
+def even_or_odd(number):
+    number = int(input())
+    if number % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
+print(even_or_odd())
