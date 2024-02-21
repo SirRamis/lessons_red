32009d5c65b05cd897a51d62b1b8104c65b9e708
Ramis <sir.ramis@mail.ru>
2024-02-21 09:05:50 +0300

    added decoreter

--- lessons_red/lessons_python/lesson_4.py
+++ lessons_red/lessons_python/lesson_4.py
@@ -1,4 +1,19 @@
 def sum_it(a, b, c):
     return a + b + c
 
-print(sum_it(5,4))
+
+print(sum_it(5, 4, c=10))
+
+
+def decorator(func):
+    def wrapper(*args):
+        print('эта функция')
+        print(f'wrap func : {func.__name__}')
+        print('wrap')
+        print(func(*args))
+        print('konex')
+    return wrapper
+
+@decorator
+def my_wrapper(item):
+    return
