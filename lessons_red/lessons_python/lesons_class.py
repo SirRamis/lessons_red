cea0ee78075300e437945a091e8e2b3edbd768e6
Ramis <sir.ramis@mail.ru>
2024-02-23 23:11:56 +0300

    added  def check_logs

---
+++ lessons_red/lessons_python/lesons_class.py
@@ -0,0 +1,25 @@
+# class Persons:
+#     def __init__(self,my_name, your_name):
+#         self.my_name = my_name
+#         self.your_name = your_name
+#
+#     def greet(self):
+#         return f'Hello {your_name}, my name is {my_name}'
+# person1 = Persons('Al','Ram')
+# print(person1.your_name)
+#
+# class Person:
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#     def prin(self):
+#         print(f'{self.first_name}')
+#
+#     def prin2(self):
+#         print(f'{self.last_name}')
+# per = Person('sdf','sdfas',45)
+# print(list(per))
+a = input()
+b = list(a)
+print(a[-1])
