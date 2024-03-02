53be3515799f2e9940c3e9a7049b7ef834b6b907
Ramis <sir.ramis@mail.ru>
2024-03-02 12:47:02 +0300

    added   2.03

---
+++ lessons_red/lessons_python/lesson_2.03.py
@@ -0,0 +1,11 @@
+class new:
+
+    def __init__(self, *args):
+        return args
+
+    itMoves = True
+    shouldItMove = True
+
+    if (itMoves):
+        if (shouldItMove):
+            print('Ok')
--- lessons_red/lessons_python/test_lesson_6_1_03.py
+++ lessons_red/lessons_python/test_lesson_6_1_03.py
@@ -38,21 +38,21 @@ def auth_token():
    yield token
 
 
-@pytest.fixture(scope="session")
-def booking_id():
-   payload = {
-       "firstname": "James",
-       "lastname": "Brown",
-       "totalprice": 150,
-       "depositpaid": True,
-       "bookingdates": {
-           "checkin": "2024-01-01",
-           "checkout": "2024-02-01"
-       },
-       "additionalneeds": "Breakfast"
-   }
-   response = requests.post(base_url, json=payload)
-   yield response.json()['bookingid']
+# @pytest.fixture(scope="session")
+# def booking_id():
+#    payload = {
+#        "firstname": "James",
+#        "lastname": "Brown",
+#        "totalprice": 150,
+#        "depositpaid": True,
+#        "bookingdates": {
+#            "checkin": "2024-01-01",
+#            "checkout": "2024-02-01"
+#        },
+#        "additionalneeds": "Breakfast"
+#    }
+#    response = requests.post(base_url, json=payload)
+#    yield response.json()['bookingid']
 
 
 # def sum_it(x, y):
@@ -63,27 +63,27 @@ def booking_id():
 #
 # def test_not_equal():
 #     assert sum_it(4, 8) == 11
-@pytest.mark.smoke
-def test_get_code():
-   result = requests.get(base_url)
-   print(result)
-   assert result.status_code == 200
+# @pytest.mark.smoke
+# def test_get_code():
+#    result = requests.get(base_url)
+#    print(result)
+#    assert result.status_code == 200
 
-@pytest.mark.skip
-def test_get_booking_by_id():
-   response = requests.get(f'{base_url}/1')
-   response_data = response.json()
-   expected_keys = [
-       "lastname",
-       "totalprice",
-       "depositpaid",
-       "bookingdates",
-       "additionalneeds"
-   ]
-   assert response.status_code == 200
-   print(response_data)
-   print(len(response_data.keys()))
-   assert len(response_data.keys()) == len(expected_keys)
+# @pytest.mark.skip
+# def test_get_booking_by_id():
+#    response = requests.get(f'{base_url}/1')
+#    response_data = response.json()
+#    expected_keys = [
+#        "lastname",
+#        "totalprice",
+#        "depositpaid",
+#        "bookingdates",
+#        "additionalneeds"
+#    ]
+#    assert response.status_code == 200
+#    print(response_data)
+#    print(len(response_data.keys()))
+#    assert len(response_data.keys()) == len(expected_keys)
    # for key in expected_keys:
    #     assert key in response_data.keys()
 
@@ -93,49 +93,48 @@ def test_get_booking_by_id():
 #     print(response.json()['bookingid'])
 #     assert response.status_code == 200
 
-@pytest.mark.regression
-@pytest.mark.xfail(reason='wrong status code')
-def test_check_created_booking(booking_id):
-   result = requests.get(f"{base_url}/{booking_id}")
-   print(result.json())
-   assert result.status_code == 200
-   assert result.json()['firstname'] == "James"
+# @pytest.mark.regression
+# @pytest.mark.xfail(reason='wrong status code')
+# def test_check_created_booking(booking_id):
+#    result = requests.get(f"{base_url}/{booking_id}")
+#    print(result.json())
+#    assert result.status_code == 200
+#    assert result.json()['firstname'] == "James"
 
 
-def test_update_booking(auth_token, booking_id):
-   payload = {
-       "firstname": "James",
-       "lastname": "Brown",
-       "totalprice": 150,
-       "depositpaid": False,
-       "bookingdates": {
-           "checkin": "2024-01-01",
-           "checkout": "2024-02-01"
-       },
-       "additionalneeds": "Lunch"
-   }
-   token = {"Cookie": f"token={auth_token}"}
+# def test_update_booking(auth_token, booking_id):
+#    payload = {
+#        "firstname": "James",
+#        "lastname": "Brown",
+#        "totalprice": 150,
+#        "depositpaid": False,
+#        "bookingdates": {
+#            "checkin": "2024-01-01",
+#            "checkout": "2024-02-01"
+#        },
+#        "additionalneeds": "Lunch"
+#    }
+#    token = {"Cookie": f"token={auth_token}"}
+#
+#    response = requests.put(f'{base_url}/{booking_id}', json=payload, headers=token)
+#    assert response.status_code == 200
+#    response_2 = requests.get(f'{base_url}/{booking_id}')
+#    print(response_2.json())
+#    assert response_2.json()["additionalneeds"] == "Lunch"
 
-   response = requests.put(f'{base_url}/{booking_id}', json=payload, headers=token)
-   assert response.status_code == 200
-   response_2 = requests.get(f'{base_url}/{booking_id}')
-   print(response_2.json())
-   assert response_2.json()["additionalneeds"] == "Lunch"
-
-def test_delete_booking(booking_id, auth_token):
-   token = {"Cookie": f"token={auth_token}"}
-   response = requests.delete(f'{base_url}/{booking_id}', headers=token)
-   assert response.status_code == 201
-   response_get = requests.get(f'{base_url}/{booking_id}')
-   assert response_get.status_code ==404
-
-
-pytest.ini
-[pytest]
-marks:
-   smoke: "for smoke tests"
-   regression: "for regression tests"
+# def test_delete_booking(booking_id, auth_token):
+#    token = {"Cookie": f"token={auth_token}"}
+#    response = requests.delete(f'{base_url}/{booking_id}', headers=token)
+#    assert response.status_code == 201
+#    response_get = requests.get(f'{base_url}/{booking_id}')
+#    assert response_get.status_code ==404
 
 
+# pytest.ini
+# [pytest]
+# marks:
+#    smoke: "for smoke tests"
+#    regression: "for regression tests"
 
-jj
+
+
